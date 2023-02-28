423b54ec5120ec1708910bcd9a37f07dc8059871
Ariel Davis <amanichai100@gmail.com>
2023-02-28 14:45:46 -0500

    slots

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,14 @@
   <h1>{{title}}</h1>
   <p>Welcome...</p>
   <div v-if="showModal">
-    <Modal :header="header" :text="text" theme="sale" @close="toggleModal"/>
+    <Modal theme="sale" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">sign up now</a>
+        <a href="#">more info</a>
+      </template>
+      <h1>Super Giveaway</h1>
+      <p>Grab your swag!</p>
+    </Modal>
   </div>
   <button @click.alt="toggleModal">Open modal (alt)</button>
 </template>
